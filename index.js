5d754664257d8e8badc43d256947f9e335dd9719
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-18 16:42:37 +0100

    finished

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import * as tools from './tools.js';
 
 const html = await tools.getHtmlFromUrl(`https://www.get-in-it.de/jobsuche?thematicPriority=5`);
-// tools.createFile('getinit.html', html);
 
 const jobs = tools.getJobsFromHtml(html);
-console.log(`${jobs.length} items`);
-jobs.forEach(job => {
-	console.log(job)
-})
+tools.createFile('jobs.json', JSON.stringify(jobs, null, 2));
+
+
